75be8e3221fa133bd265c3ce637917d1791db83f
ritaurbani <ritaurbani@gmail.com>
2025-04-07 11:46:38 +0200

    M3

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,40 @@ function App() {
   //prodotti nel carrello
   const [addedProducts, setAddedProducts] = useState([])
   console.log(addedProducts)
-  const addToCart = (product) => {
 
+  const updateProductQuantity = (name, quantity) => {
+    setAddedProducts(curr => curr.map(p => {//map(p => p.name === name? {...p, quantity} : p)
+      if(p.name === name){
+        return {
+          ...p,
+          quantity
+        }
+      }
+      return p
+    }))
+  }
+
+//   //const updateProductQuantity = (name) => {
+//   setAddedProducts(curr => curr.map(p =>
+//     p.name === name
+//       ? { ...p, quantity: p.quantity + 1 }
+//       : p
+//   ));
+// };
+
+  const addToCart = (product) => {
     const itemToAdd = addedProducts.find((item, index) => item.name === product.name)
     //se provo ad aggiungere elemento piu di una volta non si aggiunge
-    if (itemToAdd) { return }
+    if (itemToAdd) { 
+      //chiamo questa funzione quando prodotto esiste gia
+      updateProductQuantity(itemToAdd.name, itemToAdd.quantity + 1)
+      return }
     const cartItem = {
       ...product,
       quantity: 1
     }
-
     setAddedProducts([...addedProducts, cartItem])
   }
-
   // const addToCart = product => {
   //   const isProductAlreadyAdded = addedProducts.some(p => p.name === product.name);
   //   if(isProductAlreadyAdded){return}
@@ -41,7 +62,25 @@ function App() {
   //   quantity:1
   // }])
 
+  const removeFromCart = (item) => {
+    //teniamo i prodotti che non hanno il nome uguale a quello del item.name passato
+    setAddedProducts(curr => curr.filter((product,i) => product.name !== item.name))
+  }
 
+ 
+
+const calculateTotalPrice = () => {
+  let totalPriceToPay = 0
+  addedProducts.forEach((item) => {
+    totalPriceToPay += item.price*item.quantity
+  })
+  return totalPriceToPay.toFixed(2)+"$"
+}
+ //Versione con variabile e reduce
+  //calcolata a ogni modifica - Trasformare array di addedProducts in un valore, la sua somma > reduce
+  // const totalPriceToPay = addedProducts.reduce((acc, curr) => {
+  //   return acc + (curr.price * curr.quantity)
+  // },0 )
 
   return (
     <>
@@ -66,11 +105,18 @@ function App() {
                   {
                     addedProducts.map((item, index) => (
                       <li key={index}>
-                        <p>{item.name} ({item.price.toFixed(2)}$) {item.quantity} </p>
+                        <p>{item.quantity} x {item.name} ({item.price.toFixed(2)}$)</p>
+                        <button onClick={() => removeFromCart(item)}>Rimuovi dal carrello</button>
                       </li>
                     ))
                   }
                 </ul>
+                <div>
+                  {/* Se uso variabile con reduce invece di funzione */}
+                  {/* <h2>Totale da pagare {totalPriceToPay.toFixed(2)}$</h2> */}
+                  <h2>Totale da pagare</h2>
+                  {calculateTotalPrice()}
+                </div>
               </>
             )}
 
