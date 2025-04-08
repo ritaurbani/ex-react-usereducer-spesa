ec0e2e1b2b5105e37f7b437116380ac633e29f41
ritaurbani <ritaurbani@gmail.com>
2025-04-08 14:35:51 +0200

    c

--- src/M-1-2-3.jsx
+++ src/M-1-2-3.jsx
@@ -14,22 +14,39 @@ function App() {
   const [addedProducts, setAddedProducts] = useState([])
   console.log(addedProducts)
 
+                          //////////////////KEY CONCEPT/////////////
+//Se il prodotto esiste già, aumenti la sua quantità di 1 e poi lo aggiorni nel carrello
+//addToCart si occupa di aggiungere il prodotto e gestire il flusso quando il prodotto è già nel carrello (incremento della quantità).
+// updateProductQuantity è una funzione dedicata solo ad aggiornare la quantità di 
+// un prodotto già nel carrello(quando un prodotto è già presente e vogliamo cambiare la sua quantità).
+
+// User scrive in input →
+// onChange chiama updateProductQuantity →
+// updateProductQuantity aggiorna addedProducts →
+// React ricarica tutto, mostrando il nuovo item.quantity.
   const updateProductQuantity = (name, quantity) => {
     //controlli del bonus: quantiy e negativo o non e un numero
-    if (quantity < 1) {
+    if (quantity < 1 || isNaN(quantity)) {
       return;
-    }
+    }//aggiorna lo stato (il carrello) mappando tutti gli elementi correnti(la quantita)
     setAddedProducts(curr => curr.map(p => {//map(p => p.name === name? {...p, quantity} : p)
       if (p.name === name) {
         return {
           ...p,
-          quantity
+          quantity //quantity maggiorata vedi sotto
+          //La funzione updateProductQuantity è pensata per aggiornare una quantità esistente, 
+          // non per aumentarla
+        //Se scrivessi quantity + 1 dentro questa funzione, aumenteresti
+        // la quantità ogni volta che la chiami, ma l'aumento deve essere fatto 
+        // all'interno di addToCart, poiché è lì che decidi di aumentare la quantità di 1.
+        // Se il prodotto è già nel carrello, incrementi la quantità e 
+        // poi invochi updateProductQuantity per aggiornare il carrello.
         }
       }
       return p
     }))
   }
-
+  //OPZIONE CON TERNARIO
   //   //const updateProductQuantity = (name) => {
   //   setAddedProducts(curr => curr.map(p =>
   //     p.name === name
@@ -38,11 +55,18 @@ function App() {
   //   ));
   // };
 
+  //l flusso giusto
+// In addToCart, decidi di incrementare la quantità del prodotto(con itemToAdd.quantity + 1).
+// Poi invii il nuovo valore della quantità alla funzione updateProductQuantity 
+// per aggiornarlo nello stato del carrello.
   const addToCart = (product) => {
+    //potevo usare anche some
     const itemToAdd = addedProducts.find((item, index) => item.name === product.name)
     //se provo ad aggiungere elemento piu di una volta non si aggiunge
     if (itemToAdd) {
-      //chiamo questa funzione quando prodotto esiste gia
+      //chiamo questa funzione per aggiornare quantita quando prodotto esiste gia
+      //Se il prodotto è già nel carrello,incrementi la quantità 
+      // e poi invochi updateProductQuantity PER AGGIORNARE CARRELLO
       updateProductQuantity(itemToAdd.name, itemToAdd.quantity + 1)
       return
     }
@@ -51,28 +75,16 @@ function App() {
       quantity: 1
     }
     setAddedProducts([...addedProducts, cartItem])
+    // setAddedProducts(curr => [...curr, {
+    //   ...product,
+    //   quantity:1
+    // }])
   }
-  // const addToCart = product => {
-  //   const isProductAlreadyAdded = addedProducts.some(p => p.name === product.name);
-  //   if(isProductAlreadyAdded){return}
-  //   const productToAdd = {
-  //     ...product,
-  //     quantity: 1
-  //   }
-  //   setAddedProducts( curr => [...curr, productToAdd])
-  // } 
-  // //or also invece di creare variabile productToAdd, crea oggetto direttamente qui..
-  // setAddedProducts(curr => [...curr, {
-  //   ...product,
-  //   quantity:1
-  // }])
 
   const removeFromCart = (item) => {
     //teniamo i prodotti che non hanno il nome uguale a quello del item.name passato
     setAddedProducts(curr => curr.filter((product, i) => product.name !== item.name))
   }
-
-
 
   const calculateTotalPrice = () => {
     let totalPriceToPay = 0
@@ -81,7 +93,7 @@ function App() {
     })
     return totalPriceToPay.toFixed(2) + "$"
   }
-  //Versione con variabile e reduce
+  //VERSIONE CON VARIABILE E REDUCE
   //calcolata a ogni modifica - Trasformare array di addedProducts in un valore, la sua somma > reduce
   // const totalPriceToPay = addedProducts.reduce((acc, curr) => {
   //   return acc + (curr.price * curr.quantity)
@@ -115,6 +127,7 @@ function App() {
                             type='number'
                             //valore e sincronizzato con quantita
                             value={item.quantity}
+                            //onChange invoca updateProductQuantity, che aggiorna la quantità del prodotto nello stato (addedProducts).
                             onChange={(e) => updateProductQuantity(item.name, parseInt(e.target.value))} />
                           <span> x {item.name} ({item.price.toFixed(2)}$) </span>
                         </p>
